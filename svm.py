eb2d7824da347ca0fe02a0bc95c7a1c881e1a4fe
pythongiant <srihari.unnikrishnan@gmail.com>
2017-08-04 17:52:56 +0530

    More ML

---
+++ svm.py
@@ -0,0 +1,13 @@
+from sklearn import svm
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+digits=datasets.load_digits()
+#print(len(digits.data))
+#print(len(digits.target))
+clf=svm.SVC(gamma=0.001,C=100)#C== margin of error
+X,y=digits.data[:-10],digits.target[:-10]
+clf.fit(X,y)
+print(clf.predict(digits.data[5]))
+plt.imshow(digits.images[1])
+plt.show()
